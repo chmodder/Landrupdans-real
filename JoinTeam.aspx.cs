c1bf800642373f486f14fd0426c880de64c8fa6b
chmodder <hotmailgateway@gmail.com>
2014-01-14 19:31:51 +0100

    Trying to make session url variable work

--- JoinTeam.aspx.cs
+++ JoinTeam.aspx.cs
@@ -7,51 +7,54 @@ using System.Web.UI.WebControls;
 
 public partial class JoinTeam : System.Web.UI.Page
 {
+    //private string BackToLastPage;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        int BrugerId = Convert.ToInt32(Session["BrugerId"]);
-        int TeamId = Convert.ToInt32(Request.QueryString["HoldId"]);
+        //int BrugerId = Convert.ToInt32(Session["BrugerId"]);
+        //int TeamId = Convert.ToInt32(Request.QueryString["HoldId"]);
+        
+        ////SE HER DITLEV
+        //BackToLastPage = (string)Session["CurrentUrl"];
 
-        //tager den gemte url og putter den ind i en anden variabel, så sessionvariblen bliver nulstillet
-        string BackToLastPage = (string)Session["CurrentUrl"];
-        Session["CurrentUrl"] = null;
+        ////Makes new variable from saved Session url, and then resets sessionurl.
+        //Session["CurrentUrl"] = null;
 
-        //smider BackToLastPage-variablen i en viewstate variabel, så den kan bruges i en anden metode udefor pageload-metoden
-        ViewState["BackToLastPage"] = BackToLastPage;
+        //if (Session["brugerId"] != null)
+        //{
 
-        if (Session["brugerId"] != null)
-        {
+        //    if (ClassSheet.CheckIfUserAgeMatchTeamAge(BrugerId, TeamId))
+        //    {
+        //        ClassSheet.JoinATeam(BrugerId, TeamId);
 
-            if (ClassSheet.CheckIfUserAgeMatchTeamAge(BrugerId, TeamId))
-            {
-                ClassSheet.JoinATeam(BrugerId, TeamId);
+        //        if (BackToLastPage != null)
+        //        {
+        //           //Uses the new savedUrl variable to go back to last page.
+        //        Response.Redirect(BackToLastPage); 
+        //        }
+        //        else
+        //        {
+        //            Response.Redirect("Default.aspx");
+        //        }                
+        //    }
 
-                if (BackToLastPage != null)
-                {
-                   //bruger den gemte sessionvariablen til at gå tilbage til tidligere side
-                Response.Redirect(BackToLastPage); 
-                }
-                else
-                {
-                    Response.Redirect("Default.aspx");
-                }
-                
-            }
-
-            else
-            {
-                AgeNotOk.Text = "Du har ikke den rigtige alder til dette hold";
-            }
-
-        }
-        else
-        {
-            Response.Redirect("Login.aspx");
-        }
+        //    else
+        //    {
+        //        AgeNotOk.Text = "Du har ikke den rigtige alder til dette hold";
+        //    }
+        //}
+        //else
+        //{
+        //    Response.Redirect("Login.aspx");
+        //}
     }
 
+    //NOT WORKING...
     protected void BackToLastPageBtn_Click(object sender, EventArgs e)
     {
-        Response.Redirect(Convert.ToString(ViewState["BackToLastPage"]));
+        //SE HER DITLEV
+        
+        //Response.Write(BackToLastPage);
+        Response.Write((string)Session["CurrentUrl"]);
     }
 }
