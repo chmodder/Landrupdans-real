bb000e4f381877ac0e063bca51fc2a9a1fd18ee2
chmodder <hotmailgateway@gmail.com>
2014-01-14 22:18:41 +0100

    Session url variable now working
    Saved page-variable now working. Got help here:
    http://stackoverflow.com/questions/21121766/defining-and-adding-value-to-variable-from-session-variable

--- JoinTeam.aspx.cs
+++ JoinTeam.aspx.cs
@@ -7,54 +7,57 @@ using System.Web.UI.WebControls;
 
 public partial class JoinTeam : System.Web.UI.Page
 {
-    //private string BackToLastPage;
+    //This helped me http://stackoverflow.com/questions/21121766/defining-and-adding-value-to-variable-from-session-variable
+
+    //Defining Go back variable
+    private string BackToLastPage { get { return (Session["CurrentUrl"] == null) ? "" : Session["CurrentUrl"].ToString(); } }
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        //int BrugerId = Convert.ToInt32(Session["BrugerId"]);
-        //int TeamId = Convert.ToInt32(Request.QueryString["HoldId"]);
-        
-        ////SE HER DITLEV
+        int BrugerId = Convert.ToInt32(Session["BrugerId"]);
+        int TeamId = Convert.ToInt32(Request.QueryString["HoldId"]);
+
+        //Adding value to go back variable from sessionurl
         //BackToLastPage = (string)Session["CurrentUrl"];
 
-        ////Makes new variable from saved Session url, and then resets sessionurl.
-        //Session["CurrentUrl"] = null;
+        
 
-        //if (Session["brugerId"] != null)
-        //{
+        if (Session["brugerId"] != null)
+        {
 
-        //    if (ClassSheet.CheckIfUserAgeMatchTeamAge(BrugerId, TeamId))
-        //    {
-        //        ClassSheet.JoinATeam(BrugerId, TeamId);
+            if (ClassSheet.CheckIfUserAgeMatchTeamAge(BrugerId, TeamId))
+            {
+                ClassSheet.JoinATeam(BrugerId, TeamId);
 
-        //        if (BackToLastPage != null)
-        //        {
-        //           //Uses the new savedUrl variable to go back to last page.
-        //        Response.Redirect(BackToLastPage); 
-        //        }
-        //        else
-        //        {
-        //            Response.Redirect("Default.aspx");
-        //        }                
-        //    }
+                if (BackToLastPage != null)
+                {
+                    //Uses the new savedUrl variable to go back to last page.
+                    Response.Redirect(BackToLastPage);
+                }
+                else
+                {
+                    Response.Redirect("Default.aspx");
+                }
+            }
 
-        //    else
-        //    {
-        //        AgeNotOk.Text = "Du har ikke den rigtige alder til dette hold";
-        //    }
-        //}
-        //else
-        //{
-        //    Response.Redirect("Login.aspx");
-        //}
+            else
+            {
+                AgeNotOk.Text = "Du har ikke den rigtige alder til dette hold";
+            }
+        }
+        else
+        {
+            //Not saving last page. Need to find solution.
+            Response.Redirect("Login.aspx");
+        }
     }
 
     //NOT WORKING...
     protected void BackToLastPageBtn_Click(object sender, EventArgs e)
     {
-        //SE HER DITLEV
-        
-        //Response.Write(BackToLastPage);
-        Response.Write((string)Session["CurrentUrl"]);
+        //YOU SHOULD SET THE CURRENT URL TO NULL HERE.
+        string tempUrl = BackToLastPage;
+        Session["CurrentUrl"] = null;
+        Response.Redirect(tempUrl);
     }
 }
